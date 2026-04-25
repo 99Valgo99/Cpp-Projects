6f50e03f0ca6856f3be0fdc06cbf2fbfc0c422f5
99Valgo99 <224714804+99Valgo99@users.noreply.github.com>
2026-04-25 03:09:27 +0100

    Fixed a Typo..

--- Cpp M5/ex01/Form.cpp
+++ Cpp M5/ex01/Form.cpp
@@ -5,7 +5,10 @@ Form::Form() : name("Form"), marked(false), gradeSign(1), gradeExec(1) {
     std::cout << "Form Default Constructor Called..." << std::endl;
 }
 
-Form::Form(std::string newName, int newGradeSign, int newGradeExec, bool isMarked) : name(newName), marked(isMarked), gradeSign(newGradeSign), gradeExec(newGradeExec) {
+Form::Form(std::string newName, int newGradeSign, int newGradeExec, bool isMarked) :\
+ name(newName), marked(isMarked),\
+  gradeSign(newGradeSign), gradeExec(newGradeExec) {
+    
     std::cout << "Parametrized Form Constructor Is Called..." << std::endl;
     if (newGradeExec > 150 || newGradeSign > 150) throw GradeTooLowException();
     else if (newGradeExec < 1 || newGradeSign < 1) throw GradeTooHighException();
@@ -16,7 +19,10 @@ Form::~Form(){
     std::cout << "Form Destructor Called..." << std::endl;
 }
 
-Form::Form(const Form& other) : name(other.name), marked(other.marked), gradeSign(other.gradeSign), gradeExec(other.gradeExec) {
+Form::Form(const Form& other) : \
+name(other.name), marked(other.marked), \
+gradeSign(other.gradeSign), gradeExec(other.gradeExec) {
+
     std::cout << "Form Copy Constructor Called..." << std::endl;
 }
 
@@ -27,4 +33,4 @@ Form& Form::operator=(const Form& other) {
     return *this;
 }
 
-// Getters //
+// Getters
